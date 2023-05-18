380614faea6d6220d39af6dfeefe6457418cc057
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-18 15:46:08 +0200

    Added an array to get a result in console of nmbr

--- snack_1/script.js
+++ snack_1/script.js
@@ -1,12 +1,15 @@
 console.log('JS Loaded Succesfully!');
 
 let sum = 0;
+let numbersInput = [];
 
-for (let i = 0; i < 9; i++) {
-    const numberInput = parseInt(prompt("Inserisci un numero a scelta!"));
+for (let i = 1; i <= 10; i++) {
+    const numberInput = parseInt(prompt(`${i} Inserisci un numero a scelta!`));
+
+
     sum += Number(numberInput); 
+    numbersInput.push(Number(numberInput));
 }
 
-console.log(
-    `Il risulato della somma dei tuoi numeri e = ${sum}!`
-    );
+
+console.log(`Il risulato della somma dei tuoi numeri e = ${sum}!`);
