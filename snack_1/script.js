bf40ee1c71a1996349b03267e693f88a3a2ca451
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-18 15:53:43 +0200

    Added an array result to see what numbers typed

--- snack_1/script.js
+++ snack_1/script.js
@@ -6,10 +6,9 @@ let numbersInput = [];
 for (let i = 1; i <= 10; i++) {
     const numberInput = parseInt(prompt(`${i} Inserisci un numero a scelta!`));
 
-
     sum += Number(numberInput); 
     numbersInput.push(Number(numberInput));
 }
 
-
+console.log(numbersInput);
 console.log(`Il risulato della somma dei tuoi numeri e = ${sum}!`);
