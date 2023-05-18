a329ca4d3e5ae36aef9a7d42adcbf7c7118f9876
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-18 17:41:28 +0200

    fixed check for odd number

--- snack_3/script.js
+++ snack_3/script.js
@@ -6,7 +6,7 @@ const oddNumberList = [];
 for (let i = 1; i <= 6; i++) {
     const numberInput = parseInt(prompt(`${i} Inserisci un numero a scelta!`));
 
-    if (numberInput % 1 === 0 && numberInput % 2 !== 0) {
+    if (numberInput % 1 == 0 && numberInput % 2 != 0) {
         oddNumberList.push(numberInput) ; 
     }
 }
