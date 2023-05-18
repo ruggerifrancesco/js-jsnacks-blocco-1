0fdc29348e5fb24e6e8d7b069f61a327b581b670
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-18 16:13:36 +0200

    updated, removed break and put boolean flag

--- snack_1/script.js
+++ snack_1/script.js
@@ -2,20 +2,23 @@ console.log('JS Loaded Succesfully!');
 
 let sum = 0;
 let numbersInput = [];
+let errorNan = false; // Boolean variable to track errors
 
+//For Cycle for askign numebers
 for (let i = 1; i <= 10; i++) {
     const numberInput = parseInt(prompt(`${i} Inserisci un numero a scelta!`));
 
-     if (isNaN(numberInput)) {
+    if (isNaN(numberInput)) {
         console.log(`Errore di scrittura alla finestra numero ${i}`);
-        break;
-    }
+        errorNan = true; // Set the error flag to true
+      } else {
+        sum += Number(numberInput);
+        numbersInput.push(Number(numberInput));
+      }
+    }    
 
-    sum += Number(numberInput); 
-    numbersInput.push(Number(numberInput));
-}
-
-if (numbersInput.length === 10) {
+// Output If Cycle
+if (numbersInput.length === 10 && !errorNan) {
     console.log(numbersInput);
     console.log(`La somma dei numeri e di = ${sum}!`);
 } else {
