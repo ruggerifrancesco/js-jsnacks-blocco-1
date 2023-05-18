8a0680a942d2e3841664d5c8e7ce79188d19b62b
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-18 16:02:53 +0200

    Added an if and break cond. for invalid input

--- snack_1/script.js
+++ snack_1/script.js
@@ -6,13 +6,18 @@ let numbersInput = [];
 for (let i = 1; i <= 10; i++) {
     const numberInput = parseInt(prompt(`${i} Inserisci un numero a scelta!`));
 
+     if (isNaN(numberInput)) {
+        console.log(`Errore di scrittura alla finestra numero ${i}`);
+        break;
+    }
+
     sum += Number(numberInput); 
     numbersInput.push(Number(numberInput));
 }
 
 if (numbersInput.length === 10) {
     console.log(numbersInput);
-    console.log(`The result of the sum of your numbers is ${sum}!`);
+    console.log(`La somma dei numeri e di = ${sum}!`);
 } else {
-    console.log('Error: Insufficient inputs');
+    console.log('Errore: non ci sono abbastanza numeri per la somma!');
 }
