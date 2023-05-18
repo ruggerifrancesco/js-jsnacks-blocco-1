b0163da0b6c99f0aa03e144254da21b8f99b8855
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-18 17:34:03 +0200

    Defined strcutre for pushing odd numbers

--- snack_3/script.js
+++ snack_3/script.js
@@ -1,1 +1,14 @@
 console.log('JS Loaded Succesfully!');
+
+// Numer List Array
+const oddNumberList = [];
+
+for (let i = 1; i <= 6; i++) {
+    const numberInput = parseInt(prompt(`${i} Inserisci un numero a scelta!`));
+
+    if (numberInput % 1 === 0 && numberInput % 2 !== 0) {
+        oddNumberList.push(numberInput) ; 
+    }
+}
+
+console.log(oddNumberList);
