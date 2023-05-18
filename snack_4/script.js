5d98ea5dedcba93f882c0393e1406ea4786e6591
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-18 18:41:34 +0200

    Added if cycle to calculate each didgits

--- snack_4/script.js
+++ snack_4/script.js
@@ -2,3 +2,15 @@ console.log('JS Loaded Succesfully!');
 
 //Ask user a 4 digits number
 const userInput = parseInt(prompt("Inserisci un numero a 4 cifre"));
+
+// Check if the input is exactly 4 digits
+if (userInput.length !== 4) {
+    console.log("Errore, input non valid. Serve perforza u numero a 4 cifre!");
+  } else {
+    // Calculate the sum of individual digits
+    let sum = 0;
+    for (let i = 0; i < userInput.length; i++) {
+      sum += parseInt(userInput[i]);
+    }
+  
+  }
