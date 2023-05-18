3c877778ce2436c96bd0aa6d23598d04426cdd4d
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-18 16:17:04 +0200

    Defined Brithday list people

--- snack_2/script.js
+++ snack_2/script.js
@@ -1,1 +1,14 @@
 console.log('JS Loaded Succesfully!');
+
+const birthdayList = [
+    'Bartolomeo Barillaro',
+    'Romana Comolli',
+    'Lilla Morosini',
+    'Ippazio Casaleggio',
+    'Valeria Filzi',
+    'Lidia Gagliardi',
+    'Giulio Gangemi',
+    'Michela Rossi',
+    'Bruno Boito',
+    'Rosaria Solari',
+];
