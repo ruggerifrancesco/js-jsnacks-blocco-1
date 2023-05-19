05a7cdaab2798460e7a68ae5ef4b74ccf74482a1
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-19 11:12:28 +0200

    Added a flag false for an invalid input

--- snack_3/script.js
+++ snack_3/script.js
@@ -2,12 +2,18 @@ console.log('JS Loaded Succesfully!');
 
 // Numer List Array
 const oddNumberList = [];
+let isNumberNaN = false;
 
 for (let i = 1; i <= 6; i++) {
     const numberInput = parseInt(prompt(`${i} Inserisci un numero a scelta!`));
 
+    if (isNaN(numberInput)) {
+        console.error('Invalid input! Please enter a valid number.');
+        isNumberNaN = true;
+    }
+
     if (numberInput % 1 == 0 && numberInput % 2 != 0) {
-        oddNumberList.push(numberInput) ; 
+        oddNumberList.push(numberInput); 
     }
 }
 
