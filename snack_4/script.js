5b7e945dd800d827ed1d0c897e73e20b9aab8959
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-05-19 11:12:47 +0200

    new cycle for user not put any value

--- snack_4/script.js
+++ snack_4/script.js
@@ -3,16 +3,23 @@ console.log('JS Loaded Succesfully!');
 //Ask user a 4 digits number
 const userInput = prompt("Inserisci un numero a 4 cifre");
 
-// Check if the input is exactly 4 digits
-if (userInput.length !== 4) {
-    console.log("Errore, input non valido. Serve perforza un numero a 4 cifre!");
+if (userInput === "") {
+  console.log('UUtente ha premuto OK Senza Nessun valore');
+
+  // Check if the input is exactly 4 digits
+  if (userInput.length !== 4) {
+    console.error("Errore, input non valido. Serve perforza un numero a 4 cifre!");
   } else {
     // Calculate the sum of individual digits
     let sum = 0;
     for (let i = 0; i < userInput.length; i++) {
       sum += parseInt(userInput[i]);
     }
-  
+
     // Show output of the sum
     console.log(`La somma dei 4 numeri e di, ${sum}`);
   }
+
+} else {
+  console.error("Utente ha cancellato l' operazione");
+}
